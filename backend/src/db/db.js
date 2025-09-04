0321fffb67302c3c5d0355fa5f15214912911357
Naresh goud <ediganareshgoud406@gmail.com>
2025-09-04 23:14:40 +0530

    key

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://ediganareshgoud406_db_user:dNcIHnpFkPEbVic9@cluster0.9vp1v0y.mongodb.net/Gusto"
-  );
+  await mongoose.connect("");
 };
 
 connectDB();
